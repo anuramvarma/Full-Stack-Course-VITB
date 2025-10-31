2b670ca3940a09a904f4992e496b533054e6f2e3
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2025-10-31 15:13:53 +0530

    Add basic navigation between two screens

---
+++ Flutter/Navigator.dart
@@ -0,0 +1,59 @@
+
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+      home:Home(),
+  ),
+  );
+}
+class Home extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(
+      title: Text("Screen 1"),
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+    ),
+      body:
+    Center(
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 140,),
+            Text("flutter internal",style: TextStyle(
+              color: const Color.fromARGB(255, 143, 49, 244),
+              fontWeight: FontWeight.bold
+            ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context,MaterialPageRoute(builder: (context)=> About()), );
+            }, 
+            child: Text("click me",style:TextStyle(color: Colors.blue),
+            ),
+            )
+          ],
+        ),
+      ),
+    ),
+    );
+  }
+}
+class About extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(
+      title: Text("Screen2"),
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+    ),
+      body:Center(
+      child: ElevatedButton(onPressed: (){
+        Navigator.pop(context);
+      }, 
+      child: Text("click here revert back"),
+      ),
+      ),
+     );
+  }  
+}
